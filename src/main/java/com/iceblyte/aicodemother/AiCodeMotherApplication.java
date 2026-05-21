6274986666d838f9e699238782b3dee8430ddf5d
ice <2624507601@qq.com>
2026-05-21 12:32:42 +0800

    后端初始化 1. 初始化项目 2. 添加依赖和配置（HuTool和Knife4j接口文档） 3. 提供基础代码（自定义异常、请求响应包装类） 4. 提供基础接口（健康检查接口）

---
+++ src/main/java/com/iceblyte/aicodemother/AiCodeMotherApplication.java
@@ -0,0 +1,15 @@
+package com.iceblyte.aicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy(exposeProxy = true)
+@SpringBootApplication
+public class AiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/iceblyte/aicodemother/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.iceblyte.aicodemother.controller;
+
+import com.iceblyte.aicodemother.common.BaseResponse;
+import com.iceblyte.aicodemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
